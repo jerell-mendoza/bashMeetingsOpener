d0336ce4e0cf1593725957c0e0e86d7ae438133e
Jerell Mendoza <jerell.mendoza@semanticbits.com>
2022-05-02 11:09:59 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ fiveMinsAheadJustTime=$(date -v+5M +"%l:%M %p")
 currentTime=$(date +"%Y-%m-%dT%H:%M:%S%Z")
 
 nextEvent=$(/usr/local/bin/icalBuddy -ic jerell.mendoza@semanticbits.com -iep location -f eventsFrom:$currentTime to:$fiveMinsAhead |  awk -F' ' '{print $NF}' | tail -n1 | tr -d "[:cntrl:]" | cut -d"[" -f 1)
-nextEventJustTime=$(/usr/local/bin/icalBuddy -ic jerell.mendoza@semanticbits.com -iep datetime -f eventsFrom:$currentTime to:$fiveMinsAhead | tail -n1)
+nextEventJustTime=$(/usr/local/bin/icalBuddy -ic jerell.mendoza@semanticbits.com -iep datetime -f eventsFrom:$currentTime to:$fiveMinsAhead | tail -n1 | cut -d'-' -f1)
 
 if [[ "$nextEventJustTime" == *"$fiveMinsAheadJustTime"* ]]; then
   echo "opening zoom event"
